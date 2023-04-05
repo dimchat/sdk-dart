81e846921e83c551c02bcce3c46a33238a879c51
Moky <albert.moky@gmail.com>
2023-04-05 20:06:21 +0800

    sdk init

---
+++ dimsdk/lib/dimsdk.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library dimsdk;
+
+export 'src/dimsdk_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
