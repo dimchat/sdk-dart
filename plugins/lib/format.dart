2149745a4d50076e6c7496c21d4b234a74b05f90
Moky <albert.moky@gmail.com>
2025-08-04 00:47:55 +0800

    remove plugins module

--- plugins/lib/format.dart
+++
@@ -1,9 +0,0 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library dim_plugins;
-
-export 'src/format/coders.dart';
-
-export 'src/format/pnf.dart';
-export 'src/format/ted.dart';
